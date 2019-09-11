c2125ebea93f433402100fc668c17b22779fc4cf
chibko <info@chibko.com>
2019-09-11 14:37:42 +0200

    Move script to the bottom of the page if boostrap is activated

--- src/FontAwesome.php
+++ src/FontAwesome.php
@@ -7,7 +7,7 @@ class FontAwesome
     public function checkFontAwesome(\PageModel $objPage, \LayoutModel $objLayout, \PageRegular $objPageRegular)
     {
         if ($objLayout->fontawesome) {
-            if ($objLayout->boostrap) {
+            if ($objLayout->bootstrap) {
                 $GLOBALS['TL_JS_EXT'][] = $objLayout->fontawesome_kiturl;
             } else {
                 $GLOBALS['TL_HEAD'][] = $objLayout->fontawesome_kiturl;
